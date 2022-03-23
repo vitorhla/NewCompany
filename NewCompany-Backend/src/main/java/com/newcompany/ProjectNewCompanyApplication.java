29c01bdc5b33407523f7869de0cf976ca283a4b5
vitorhla <vitorhenriquealt@gmail.com>
2022-03-23 20:19:21 -0300

    Commit Inicial

---
+++ NewCompany-Backend/src/main/java/com/newcompany/ProjectNewCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.newcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNewCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNewCompanyApplication.class, args);
+	}
+
+}
