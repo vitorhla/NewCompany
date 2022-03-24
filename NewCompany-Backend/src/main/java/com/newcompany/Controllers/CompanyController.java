bf74aa2cd311feae8a4d2a21afb24f487d43f433
vitorhla <vitorhenriquealt@gmail.com>
2022-03-23 22:37:37 -0300

    tratamento de excecao

--- NewCompany-Backend/src/main/java/com/newcompany/Controllers/CompanyController.java
+++ NewCompany-Backend/src/main/java/com/newcompany/Controllers/CompanyController.java
@@ -5,25 +5,32 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.newcompany.dto.CompanyDTO;
 import com.newcompany.services.CompanyService;
+import com.newcompany.services.exceptions.ControllerNotFoundException;
 
 @RestController
 @RequestMapping(value = "/company")
 public class CompanyController {
-	
-	
+
 	@Autowired
 	private CompanyService service;
-	
-	
-	@GetMapping(value = "/listcompany")
-	public ResponseEntity<List<CompanyDTO>> finAll() {
+
+	@GetMapping
+	public ResponseEntity<List<CompanyDTO>> findAll() {
 		List<CompanyDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CompanyDTO> finByIdCompany(@PathVariable Long id) {
+		CompanyDTO dto = service.findByIdCompany(id);
+		return ResponseEntity.ok().body(dto);
 
 	}
+
 }
--- NewCompany-Backend/src/main/java/com/newcompany/services/CompanyService.java
+++ NewCompany-Backend/src/main/java/com/newcompany/services/CompanyService.java
@@ -1,6 +1,7 @@
 package com.newcompany.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.newcompany.dto.CompanyDTO;
 import com.newcompany.entities.Company;
 import com.newcompany.repositories.CompanyRepository;
+import com.newcompany.services.exceptions.ControllerNotFoundException;
 
 @Service
 public class CompanyService {
@@ -18,10 +20,16 @@ public class CompanyService {
 	private CompanyRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<CompanyDTO> findAll() {	
-		List<Company> list  = repository.findAll();
-				return list.stream().map(x -> new CompanyDTO(x)).collect(Collectors.toList());
-		
+	public List<CompanyDTO> findAll() {
+		List<Company> list = repository.findAll();
+		return list.stream().map(x -> new CompanyDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CompanyDTO findByIdCompany(Long id) {
+		Optional<Company> obj =  repository.findById(id);
+		Company entity = obj.orElseThrow(()-> new ControllerNotFoundException("Nenhum Registro encontrado"));
+		return new CompanyDTO(entity);
 	}
 
 }
